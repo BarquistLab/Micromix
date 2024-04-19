1f2afd393ca64d47028ee78ef2a0f3c7268679bf
reganhayward <reganhayward@gmail.com>
2024-04-19 14:40:02 +0200

    Update Heatmap.py

--- Website/backend/plugins/Heatmap.py
+++ Website/backend/plugins/Heatmap.py
@@ -1,5 +1,5 @@
 def main(parameters):
-    upload_url = "http://127.1.1.1:8081/"
+    upload_url = "http://127.0.0.1:8081/"
     print("heatmap.py")
     print(parameters)
     return upload_url+'?config='+str(parameters["db_entry_id"])
