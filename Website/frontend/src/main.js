2826a670574d2e0898ed6e1fa91664b52617a0fa
reganhayward <reganhayward@gmail.com>
2024-04-23 13:32:19 +0200

    Remove @babel polyfils

--- Website/frontend/src/main.js
+++ Website/frontend/src/main.js
@@ -2,11 +2,9 @@ import Vue from 'vue'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import App from './App.vue'
 
-import '@babel/polyfill'
 import 'mutationobserver-shim'
 
 import './plugins/bootstrap-vue'
-
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
